49b00aaa5a3e55c83e4b9606fbbc82332d956edd
Javier Cejudo <javier@javiercejudo.com>
2015-08-12 00:33:26 +1000

    next-gen

--- test/presets.js
+++ test/presets.js
@@ -5,7 +5,7 @@
 require('should');
 
 var rescale = require('rescale')(require('floating-adapter')).rescale;
-var temp = require('../src/linear-presets-temperature');
+var temp = require('linear-preset-factory')(require('../src/linear-presets-temperature'));
 
 function convert(x, preset) {
   return rescale(x, preset[0], preset[1]);
@@ -17,20 +17,20 @@ function invert(preset) {
 
 describe('temperature presets', function() {
   it('should convert correctly', function() {
-    (40).should.be.exactly(convert(104, invert(temp.celsiusToFahrenheit)), 'celsiusToFahrenheit')
-      .and.exactly(convert(313.15, invert(temp.celsiusToKelvin)), 'celsiusToKelvin')
-      .and.approximately(convert(563.67, invert(temp.celsiusToRankine)), 10e-14, 'celsiusToRankine')
-      .and.exactly(convert(90, invert(temp.celsiusToDelisle)), 'celsiusToDelisle')
-      .and.exactly(convert(13.2, invert(temp.celsiusToNewton)), 'celsiusToNewton')
-      .and.exactly(convert(32, invert(temp.celsiusToReaumur)), 'celsiusToReaumur')
-      .and.exactly(convert(28.5, invert(temp.celsiusToRomer)), 'celsiusToRomer');
+    (40).should.be.exactly(convert(104, invert(temp.celsius_fahrenheit)), 'celsius_fahrenheit')
+      .and.exactly(convert(313.15, invert(temp.celsius_kelvin)), 'celsius_kelvin')
+      .and.approximately(convert(563.67, invert(temp.celsius_rankine)), 10e-14, 'celsius_rankine')
+      .and.exactly(convert(90, invert(temp.celsius_delisle)), 'celsius_delisle')
+      .and.exactly(convert(13.2, invert(temp.celsius_newton)), 'celsius_newton')
+      .and.exactly(convert(32, invert(temp.celsius_reaumur)), 'celsius_reaumur')
+      .and.exactly(convert(28.5, invert(temp.celsius_romer)), 'celsius_romer');
 
-    (0).should.be.exactly(convert(32, invert(temp.celsiusToFahrenheit)), 'celsiusToFahrenheit')
-      .and.exactly(convert(273.15, invert(temp.celsiusToKelvin)), 'celsiusToKelvin')
-      .and.exactly(convert(491.67, invert(temp.celsiusToRankine)), 'celsiusToRankine')
-      .and.exactly(convert(150, invert(temp.celsiusToDelisle)), 'celsiusToDelisle')
-      .and.exactly(convert(0, invert(temp.celsiusToNewton)), 'celsiusToNewton')
-      .and.exactly(convert(0, invert(temp.celsiusToReaumur)), 'celsiusToReaumur')
-      .and.exactly(convert(7.5, invert(temp.celsiusToRomer)), 'celsiusToRomer');
+    (0).should.be.exactly(convert(32, invert(temp.celsius_fahrenheit)), 'celsius_fahrenheit')
+      .and.exactly(convert(273.15, invert(temp.celsius_kelvin)), 'celsius_kelvin')
+      .and.exactly(convert(491.67, invert(temp.celsius_rankine)), 'celsius_rankine')
+      .and.exactly(convert(150, invert(temp.celsius_delisle)), 'celsius_delisle')
+      .and.exactly(convert(0, invert(temp.celsius_newton)), 'celsius_newton')
+      .and.exactly(convert(0, invert(temp.celsius_reaumur)), 'celsius_reaumur')
+      .and.exactly(convert(7.5, invert(temp.celsius_romer)), 'celsius_romer');
   });
 });
