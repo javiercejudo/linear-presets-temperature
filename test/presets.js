d22b6053fc8735446d518f35793652b6a08f7534
Javier Cejudo <javier@javiercejudo.com>
2015-09-27 23:04:36 +1000

    test: identity preset

--- test/presets.js
+++ test/presets.js
@@ -18,7 +18,8 @@ function invert(preset) {
 
 describe('temperature presets', function() {
   it('should convert correctly', function() {
-    (40).should.be.exactly(convert(104, invert(temp.celsius_fahrenheit)), 'celsius_fahrenheit')
+    (40).should.be.exactly(convert(40, invert(temp.celsius_celsius)), 'celsius_celsius')
+      .and.exactly(convert(104, invert(temp.celsius_fahrenheit)), 'celsius_fahrenheit')
       .and.exactly(convert(313.15, invert(temp.celsius_kelvin)), 'celsius_kelvin')
       .and.approximately(convert(563.67, invert(temp.celsius_rankine)), 10e-14, 'celsius_rankine')
       .and.exactly(convert(90, invert(temp.celsius_delisle)), 'celsius_delisle')
@@ -26,7 +27,8 @@ describe('temperature presets', function() {
       .and.exactly(convert(32, invert(temp.celsius_reaumur)), 'celsius_reaumur')
       .and.exactly(convert(28.5, invert(temp.celsius_romer)), 'celsius_romer');
 
-    (0).should.be.exactly(convert(32, invert(temp.celsius_fahrenheit)), 'celsius_fahrenheit')
+    (0).should.be.exactly(convert(0, invert(temp.celsius_celsius)), 'celsius_celsius')
+      .and.exactly(convert(32, invert(temp.celsius_fahrenheit)), 'celsius_fahrenheit')
       .and.exactly(convert(273.15, invert(temp.celsius_kelvin)), 'celsius_kelvin')
       .and.exactly(convert(491.67, invert(temp.celsius_rankine)), 'celsius_rankine')
       .and.exactly(convert(150, invert(temp.celsius_delisle)), 'celsius_delisle')
